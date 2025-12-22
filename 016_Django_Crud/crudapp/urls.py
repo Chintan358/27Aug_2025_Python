e51181d8b8ece3d7484dbeb77ac46e0049eb850d
chintan <chintan@gmail.com>
2025-12-22 14:51:46 +0530

    m

--- 016_Django_Crud/crudapp/urls.py
+++ 016_Django_Crud/crudapp/urls.py
@@ -3,5 +3,7 @@ from crudapp.views import *
 urlpatterns = [
     path("",index,name="index"),
     path("register",register,name="register"),
-    path("display",display,name="display")
+    path("display",display,name="display"),
+    path("delete",delete_student,name="delete"),
+    path("update",update_student,name="update")
 ]
--- 016_Django_Crud/crudapp/views.py
+++ 016_Django_Crud/crudapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from crudapp.models import *
 # Create your views here.
 def index(request):
@@ -7,13 +7,36 @@ def index(request):
 def register(request):
     if request.method=='POST':
         data = request.POST
+        id = data.get("id")
         name = data.get("name")
         email = data.get("email")
         phone = data.get("phone")
         age = data.get("age")
-        Student.objects.create(name=name,email=email,phone=phone,age=age)
-    return render(request,'index.html',{"msg":"Registration successfully !!!"})
+        if id :
+            st  = Student.objects.get(pk=id)
+            st.name = name
+            st.email = email
+            st.phone = phone
+            st.age = age
+            st.save()
+            return render(request,'index.html',{"msg":"Update successfully !!!"})
+
+        else :
+           
+            Student.objects.create(name=name,email=email,phone=phone,age=age)
+            return render(request,'index.html',{"msg":"Registration successfully !!!"})
 
 def display(request):
     students = Student.objects.all()
     return render(request,"display.html",{"students":students})
+
+def delete_student(request):
+    did = request.GET['did']
+    st = Student.objects.get(pk=did)
+    st.delete()
+    return redirect("display")
+
+def update_student(request):
+    uid = request.GET['uid']
+    st = Student.objects.get(pk=uid)
+    return render(request,"index.html",{"st":st})
