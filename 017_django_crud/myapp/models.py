84a3827237cac09eb4fce29e294e678b2855df84
chintan <chintan@gmail.com>
2025-12-26 14:50:00 +0530

    all

--- 017_django_crud/myapp/models.py
+++ 017_django_crud/myapp/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
-    def __str__(self):
-        return self.name
+    # def __str__(self):
+    #     return self.name
 
 
 class Product(models.Model):
--- 017_django_crud/myapp/urls.py
+++ 017_django_crud/myapp/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from myapp.views import *
 urlpatterns = [
     path("",index,name="index"),
+    path("register",register,name="register"),
+    path('display',display,name="display")
 ]
