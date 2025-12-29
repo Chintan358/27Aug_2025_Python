383abe3a47c2f8b07ca3ec6d4a8685bd933986b2
chintan <chintan@gmail.com>
2025-12-29 15:09:56 +0530

    all

--- 017_django_crud/myproject/settings.py
+++ 017_django_crud/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-49or#tt36gabxfjwnruv12xw#3r^w0k9k=kayicz&2$0sdt=np
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['13.235.73.253']
 
 
 # Application definition
